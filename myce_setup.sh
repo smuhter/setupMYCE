c053c4fe464e9a0455bd60febb8142aeb8fe7ea9
MYCE <34510750+mycelliumcoin@users.noreply.github.com>
2018-03-10 00:57:18 +0000

    simple initial bash

--- myce_setup.sh
+++ myce_setup.sh
@@ -2,6 +2,8 @@
 
 cd ~
 
+echo && echo && echo
+echo "*********************           MYCE           ***********************"
 echo "*********************  I'm here to help you :) ***********************"
 echo && echo && echo
 
@@ -40,49 +42,10 @@ git clone https://github.com/mycelliumcoin/MycelliumMN
 cd MycelliumMN/src/leveldb && chmod 777 * && cd .. && make -f makefile.unix
 
 
-CONF_DIR=~/.Myce/
-CONF_FILE=Myce.conf
-
-echo "rpcuser=myce"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
-echo "rpcpassword=mycelove"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
-echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
-echo "listen=1" >> $CONF_DIR/$CONF_FILE
-echo "server=1" >> $CONF_DIR/$CONF_FILE
-echo "daemon=1" >> $CONF_DIR/$CONF_FILE
-echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
-echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
-echo "" >> $CONF_DIR/$CONF_FILE
-
-
-cd ~/MycelliumMN/src
-./myced
-echo "PRESS ENTER[] to continue"
-read
-
 echo ""
 echo "**********************************************************************"
 echo ""
-echo "Let's configure your masternode..."
-echo "Type the IP of this server (it's located on your dashboard) then press [ENTER]:"
-hostname -I
-read IP
-echo ""
-echo "**********************************************************************"
-echo ""
-echo "Type the PORT your going to use on this server (port: 23511 recommended for MYCE) then press [ENTER]:"
-read PORT
-
-./myced masternode genkey
-echo "This is you Private Key (copy and paste it here, then press ENTER[])"
-read PRIVKEY
-./myced stop
-echo "Now copy and paste it somewhere safe, then press ENTER[]"
-read 
-
-echo ""
-echo "**********************************************************************"
-echo ""
-echo "                          Thanks I'll continue                        "
+echo "                      This should exit without errors                 "
 echo ""
 echo "**********************************************************************"
 echo ""
@@ -90,14 +53,3 @@ echo ""
 
 
 
-
-echo "" >> $CONF_DIR/$CONF_FILE
-echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
-echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
-echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
-echo "masternode=1" >> $CONF_DIR/$CONF_FILE
-echo "" >> $CONF_DIR/$CONF_FILE
-sudo ufw allow $PORT/tcp
-sudo ufw enable
-
-./myced -daemon
